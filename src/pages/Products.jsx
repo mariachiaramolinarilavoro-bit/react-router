390705b7b8d0d055873b21061f5279d45e5db662
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-03-25 21:04:19 +0100

    add products through the fetch

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react"
+
 export default function Products() {
 
-    return (
-        
-        <h1>Products</h1>
-    )
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <p>Caricamento in corso prodotti...</p>
+
+  return (
+    <>
+      <h1>Products</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>€ {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
